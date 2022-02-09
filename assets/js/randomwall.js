aecb6e125ff22b2a14379544b2844da6c191fa24
HeadshotDeluxxe <99046988+HeadshotDeluxxe@users.noreply.github.com>
2022-02-09 12:56:00 +0000

    Delete assets directory

--- assets/js/randomwall.js
+++
@@ -1,5 +0,0 @@
-const WALLPAPERS = [
-    //'solid-blue.jpg',
-    'windows-light.jpg'
-];
-document.querySelector('body').style.backgroundImage = ('url("./assets/img/wallpapers/' + (WALLPAPERS.sort(() => Math.random() - 0.5)[0]) + '")');
